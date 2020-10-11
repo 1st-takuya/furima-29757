f720fbe99765ba40a449b30307b935d412ebd610
takuya905 <kuwa.905@icloud.com>
2020-10-11 18:53:12 +0900

    商品出品機能の実装（再2）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,25 +29,50 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
+    it "カテゴリーの情報が未選択ではないこと" do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 0")
+    end
     it "商品の状態についての情報が必須であること" do
       @item.quality_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Quality can't be blank")
+    end
+    it "商品の状態についての情報が未選択ではないこと" do
+      @item.quality_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Quality must be other than 0")
     end
     it "配送料の負担についての情報が必須であること" do
       @item.postage_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Postage can't be blank")
     end
+    it "配送料の負担についての情報が未選択ではないこと" do
+      @item.postage_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Postage must be other than 0")
+    end
     it "発送元の地域についての情報が必須であること" do
       @item.shipping_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping can't be blank")
     end
+    it "発送元の地域についての情報が未選択ではないこと" do
+      @item.shipping_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping must be other than 0")
+    end
     it "発送までの日数についての情報が必須であること" do
       @item.day_id = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Day can't be blank")
+    end
+    it "発送までの日数についての情報が未選択ではないこと" do
+      @item.day_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day must be other than 0")
     end
     it "価格についての情報が必須であること" do
       @item.price = ""
