06900aa212256e78599bc0470fcfc54d9ec0541d
takuya905 <kuwa.905@icloud.com>
2020-10-13 13:29:20 +0900

    商品詳細機能の実装（再）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :create, :new, :show, :edit]
+  resources :items, only: [:index, :create, :new, :show, :edit, :update]
   resources :users, only: :new
   root to: "items#index"
 end
