73d28def88ee356a7ec793dac29f2d9b8737d967
takuya905 <kuwa.905@icloud.com>
2020-10-13 12:14:45 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   #before_action :authenticate_user!, only: :new
+  before_action :set_item, only: [:edit, :show]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -18,10 +19,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :quality_id, :postage_id, :shipping_id, :day_id, :price, :image ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :create, :new]
+  resources :items, only: [:index, :create, :new, :show, :edit]
   resources :users, only: :new
   root to: "items#index"
 end
